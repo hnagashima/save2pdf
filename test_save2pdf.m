d09e183b7f3f37b8b89cc855956081ce1afc8059
Hiroki Nagashima <hnagashima.r@gmail.com>
2020-04-19 09:36:05 +0900

    test_save2pdf runs save2pdf function, to check bugs.

---
+++ test_save2pdf.m
@@ -0,0 +1,11 @@
+clf;
+
+f = figure(1);
+subplot(1,2,1);
+x = -2*pi:.01: 2*pi;
+y1 = sin(x);
+y2 = sin(x) .* exp(-x/pi);
+plot(x,y1);
+subplot(1,2,2);
+plot(x,y2);
+save2pdf_transparent(gcf,'untitled2.pdf');
