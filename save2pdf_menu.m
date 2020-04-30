fbd3acf0855157e876116f5a12ac65001e757464
Hiroki Nagashima <hnagashima.r@gmail.com>
2020-05-01 00:59:41 +0900

    save2pdf menuを追加

--- save2pdf_menu.m
+++ save2pdf_menu.m
@@ -1,11 +1,21 @@
-function save2pdf_menu(src,event)
+function save2pdf_menu(src,event,menuhandle)
 % Add save2pdf menu to figure menubar.
 %
-% This function is called when figure is created.
+% save2pdf_menu(src, event,menuhandle)
+% - src :
+% - event :
+% - menuhandle : parent handle. obtained by ( = uimenu(figure_handle)).
+%
+% menuhandle specifies parent uimenu handle.
+% If menuhandle is not given, parent uimenu is 'exportPDF'.
+%
+% This function should be called when figure is created.
 % Run following script at first.
 % set(0, 'DefaultFigureCreateFcn',@save2pdf_menu);
-% 
+%
 
+
+narginchk(1,3);
 f = src;
 % Check if figure is numberless ( = appdesigner or some GUI apps)
 if isempty(f.Number)
@@ -17,9 +27,12 @@ end
 
 
 %% add menu
-
-mh = uimenu(f,'Text' ,'exportPDF');
-mh.HandleVisibility = 'off'; % undeletable object.
+if nargin < 3
+    mh = uimenu(f,'Text' ,'exportPDF');
+    mh.HandleVisibility = 'off'; % undeletable object.
+else
+    mh = menuhandle;
+end
 
 % —á
 % eh0 = uimenu(mh,'text','test');
@@ -27,14 +40,43 @@ mh.HandleVisibility = 'off'; % undeletable object.
 % eh0.MenuSelectedFcn = fun0;
 
 % src event is reserved inputs.
-% You need to specify as a 1st and 2nd inputs, 
+% You need to specify as a 1st and 2nd inputs,
 % even though you don't need src and event.
 
 eh1 = uimenu(mh,'text','Export to PDF');
-fun = @(src,event,varargin) save2pdf(mh.Parent);
-eh1.MenuSelectedFcn = fun;
+eh1.MenuSelectedFcn = @(src,event,varargin) save2pdf(mh.Parent);
 
 eh2 = uimenu(mh, 'text', 'Export to transparent PDF');
-fun = @(src,event, varargin) save2pdf_transparent(mh.Parent);
-eh2.MenuSelectedFcn = fun;
+eh2.MenuSelectedFcn = @(src,event, varargin) save2pdf_transparent(mh.Parent);
+
+eh3 = uimenu(mh, 'text', 'Save Figure');
+eh3.MenuSelectedFcn = @(src, event, varargin) saveAsFig(mh.Parent);
+eh3.Accelerator = 'T';
+
+    function saveAsFig(fig,~)
+        
+        % save dialog
+        if isempty(fig.FileName) == 0
+            initialTitle = fig.FileName;
+        else
+            initialTitle = 'untitled.fig';
+        end
+        [fname, path] = uiputfile( initialTitle, 'save figure as .fig');
+        if fname ~= 0
+            % remove original UI menu from saved fig file.
+            delete(mh);
+            
+            % save file
+            savefig(fig, fullfile(path,fname));
+            
+            % Restore original menu
+            createFcn = get(0,'defaultFigureCreateFcn');
+            createFcn(fig);
+        end
+        
+        
+    end
+
+f.CreateFcn = [];% reset default figure function.
+
 end
